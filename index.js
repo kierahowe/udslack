250f5fc93c50d7ebe42f2c7cc7ef8fb3b1840010
Kiera Howe <kiera.howe@10up.com>
2017-02-20 17:03:01 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,2 @@
+require('./udemail');
+module.exports = udemail;
---
+++ udslack.js
@@ -0,0 +1,13 @@
+
+
+function udemail () { 
+	this.anumber = 1;
+}
+
+udemail.prototype.registerPlugin = function (callback) { 
+
+}
+
+
+
+module.exports = udemail;	
